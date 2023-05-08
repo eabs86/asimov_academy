5ac0a4b929647505644b18955c892ca89bbfc9bf
Emmanuel Santos <emmanuel.andrade@gmail.com>
2023-05-08 15:33:35 -0300

    adição de alguns arquivos

---
+++ atividades/exemplo.py
@@ -0,0 +1,13 @@
+meu_dicionario = {"registro": "0001", 
+                  "produto":"notebook", 
+                  "modelo":"Macbook", 
+                  "conexões":["usb-c", "usb", "p", "hdmi"]
+}
+
+meu_dicionario['perifericos']=["mouse","teclado"]
+
+lista_1 = [1, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7]
+
+lista_2 = [2, 4, 6, 8, 10]
+
+set(lista_1) # retorna elementos únicos.
